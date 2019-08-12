b3218927509bcabd476fac36858d3bb9cbdf697b
League Student <league@iMac-11.attlocal.net>
2019-08-11 17:27:11 -0700

    finished Gridworld

--- src/_06_gridworld/GridworldRunner.java
+++ src/_06_gridworld/GridworldRunner.java
@@ -1,7 +1,9 @@
 package _06_gridworld;
 import java.awt.Color;
+import java.util.Random;
 
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
@@ -14,7 +16,28 @@ Bug insect = new Bug();
 Location place = new Location(2, 4);
 hello.add(place, insect);
 Bug secondinsect = new Bug();
-Location secondplace = new Location(3, 6);
+int x = new Random().nextInt(10);
+int y = new Random().nextInt(10);
+Location secondplace = new Location(x, y);
+hello.add(secondplace, secondinsect);
+insect.setColor(new Color(0, 217, 255));
+insect.setDirection(90);
+Flower hi = new Flower();
+Flower secondhi = new Flower();
+Location thirdplace = new Location(2,5);
+Location fourthplace = new Location(2,3);
+hello.add(thirdplace, hi);
+hello.add(fourthplace, secondhi);
+for (int i = 0; i <96;  i++) {
+hello.add(hello.getRandomEmptyLocation(), new Flower());
+	
+}
+
+
+
+
+
+
 
 
 
